0de59fe63c2bbbfcfbc2d353f0d29bf740850cd0
Efe Cihan Zengin <112198832+efecihanzengin@users.noreply.github.com>
2025-02-05 20:25:53 +0300

    skills done, minor changes

--- portfolio-project/src/context/LanguageContext.jsx
+++ portfolio-project/src/context/LanguageContext.jsx
@@ -1,6 +1,7 @@
-import React, { createContext, useState, useContext, useEffect } from "react";
+import { createContext, useState, useEffect } from "react";
 import languagesData from "../data";
 import axios from "axios";
+import PropTypes from "prop-types";
 import { toast } from "react-toastify";
 
 const LanguageContext = createContext();
@@ -12,51 +13,56 @@ export const LanguageProvider = ({ children }) => {
   } else {
     lang = "tr";
   }
-};
-
-const [language, setLanguage] = useState(lang);
-  const [translations,setTranslations]=useState({...languagesData});
+  const [language, setLanguage] = useState(lang);
+  const [translations, setTranslations] = useState({ ...languagesData });
   const [loading, setLoading] = useState(true);
+
   useEffect(() => {
     const fetchTranslations = async () => {
       try {
-        const response = await axios.post('https://reqres.in/api/workintech', languagesData[language]);
+        const response = await axios.post(
+          "https://reqres.in/api/workintech",
+          languagesData[language]
+        );
         setTranslations(response.data);
-        if(!loading){
-          if(language==="tr"){
-            toast.success("Dil değişti.")
-          }else{
-            toast.success("Language changed.")
+        if (!loading) {
+          if (language === "tr") {
+            toast.success("Dil değişti.");
+          } else {
+            toast.success("Language changed.");
           }
         }
-    } catch (error) {
-        console.error('Error fetching translations:', error);
-        toast.error(error)
+      } catch (error) {
+        console.error("Error fetching translations:", error);
+        toast.error(error);
       } finally {
         setLoading(false);
-        if(language==="tr"){
-          toast.success("Sayfa yüklendi!")
-        }
-        else{
-          toast.success("Page loaded!")
+        if (language === "tr") {
+          toast.success("Sayfa yüklendi!");
+        } else {
+          toast.success("Page loaded!");
         }
       }
     };
     fetchTranslations();
-  }, [language]);
-  
+  }, [language, loading]);
+
   if (loading) {
-    return <h1 className='text-sky-800 font-semibold flex  text-center justify-self-center place-self-center'>LOADING.....</h1>;
-  } 
+    return (
+      <h1 className="text-sky-800 font-semibold flex  text-center justify-self-center place-self-center">
+        LOADING..
+      </h1>
+    );
+  }
+
   const translate = (key) => translations[key] || key;
+
   return (
     <LanguageContext.Provider value={{ language, setLanguage, translate }}>
       {children}
     </LanguageContext.Provider>
   );
 };
-
-export const useLanguage = () => {
-  const context = useContext(LanguageContext);
-  return context;
+LanguageProvider.propTypes = {
+  children: PropTypes.node.isRequired,
 };
