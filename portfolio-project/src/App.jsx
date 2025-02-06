1ac7f203ea3eb0d90550878618a346b7c2a771f2
Efe Cihan Zengin <112198832+efecihanzengin@users.noreply.github.com>
2025-02-06 14:34:22 +0300

    commit before node_module reinstall

--- portfolio-project/src/App.jsx
+++ portfolio-project/src/App.jsx
@@ -1,29 +1,34 @@
-import './App.css';
-import './global.css';
+import './App.css'
+import './global.css'
+import React, { useEffect, useState } from "react";
+import ModeComponent from './components/ModeComponent';
 import { LanguageProvider } from './context/LanguageContext';
+import languagesData from './data';
+import axios from 'axios';
+import LanguageChanger from './components/LanguageChanger';
 import Header from './components/Header';
 import Skills from './components/Skills';
 import Profile from './components/Profile';
 import Projects from './components/Projects';
 import Footer from './components/Footer';
-import ModeComponent from './components/ModeComponent';
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-
+import { ToastContainer, toast } from 'react-toastify';
+  import 'react-toastify/dist/ReactToastify.css';
 function App() {
+
+
   return (
     <>
-      <LanguageProvider>
-        <ToastContainer />
+      <LanguageProvider >
+      <ToastContainer />
         <Header />
-        <ModeComponent /> {/* Eğer kullanmak istemiyorsan bu satırı kaldır */}
         <Skills />
-        <Profile />
-        <Projects />
-        <Footer />
+        <Profile/>
+        <Projects/>
+        <Footer/>
       </LanguageProvider>
     </>
-  );
+  )
 }
 
-export default App;
+
+export default App
--- portfolio-project/tailwind.config.js
+++ portfolio-project/tailwind.config.js
@@ -1,11 +1,34 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-} 
+	content: [
+	  "./index.html",
+	  "./src/**/*.{js,ts,jsx,tsx}",
+	],
+	darkMode: "class",
+	theme: {
+	  borderWidth: {
+	    DEFAULT: '1px',
+	    '0': '0',
+	    '2': '2px',
+	    '3': '3px',
+	    '4': '4px',
+	    '6': '6px',
+	    '8': '8px',
+	    '16': '16px',
+	  },
+	  extend: {
+	   colors:{
+	    'button-org-color':'#E92577',
+	    
+	   },
+	   fontFamily: {
+	    Inter: ["Inter", "sans-serif"],
+	    playfair:["Playfair Display","sans-serif"]
+	  },
+	   screens: {
+	    'xs': '380px',
+	  },
+	  },
+	},
+	plugins: [],
+    }
